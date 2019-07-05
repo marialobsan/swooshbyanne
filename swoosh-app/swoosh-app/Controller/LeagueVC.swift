72563642bc444b53da83e4b854c478e5efec3aef
Anne Saints <annesaints@Annes-iMac.local>
2019-07-05 21:06:21 +0800

    IBActions and Data Models

--- swoosh-app/swoosh-app/Controller/LeagueVC.swift
+++ swoosh-app/swoosh-app/Controller/LeagueVC.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+ 
+    var player: Player!
+    //we have a model that can stores data
+    
+    
+    @IBOutlet weak var nxtBtn: BorderButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player ()
+        //initialize the struct
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
@@ -22,6 +31,23 @@ class LeagueVC: UIViewController {
     
     }
     
+    @IBAction func onMenTapped(_ sender: Any) {
+    selectLeague(leagueType: "men")
+    }
     
+    @IBAction func onWomenTapped(_ sender: Any) {
+    selectLeague(leagueType: "women")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+    selectLeague(leagueType: "co-ed")
+    }
+
+    
+    func selectLeague (leagueType: String) {
+        player.desiredLeague = leagueType
+        nxtBtn.isEnabled = true
+        
+    }
 
 }
---
+++ swoosh-app/swoosh-app/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  swoosh-app
+//
+//  Created by Anne Saints on 05/07/2019.
+//  Copyright © 2019 Anne Saints. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var  selectedSkillLevel: String?
+    
+}
